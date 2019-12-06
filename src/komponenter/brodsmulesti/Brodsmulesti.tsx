e1d96ae8e5586b20eeb85bd61b56f141addc101c
Thomas Flemming <thomas.flemming@gmail.com>
2019-12-06 12:32:59 +0100

    Ny brødsmulesti

--- src/komponenter/brodsmulesti/Brodsmulesti.tsx
+++ src/komponenter/brodsmulesti/Brodsmulesti.tsx
@@ -1,17 +1,60 @@
 import * as React from "react";
 import "./brodsmulesti.less";
+import NavFrontendChevron from 'nav-frontend-chevron';
 import {onClickLink} from "../../utils/navigasjon";
+import useWindowSize from "../../utils/useWindowSize";
 
-const Brodsmulesti: React.FC<{tittel: string}> = ({tittel}) => {
-    return (
-        <div className="brodsmulesti">
-            <a href=".."
-               onClick={(event: any) => onClickLink(event, "/")}
+const Brodsmulesti: React.FC<{ tittel: string }> = ({tittel}) => {
+    const {width} = useWindowSize();
+    const frontpageUrl = `/`;
+
+    let crumbs: React.ReactNode = (
+        <>
+            <div key="tilbake" className="typo-normal breadcrumbs__item">
+                <a href=".."
+                   onClick={(event: any) => onClickLink(event, frontpageUrl)}
+                   title="Gå til forrige side"
+                >
+                    Økonomisk sosialhjelp
+                </a>
+            </div>
+            <div key="chevron" aria-hidden={true}>
+                <NavFrontendChevron type="høyre"/>
+            </div>
+            <div aria-current="page"
+               key="currentPage"
+               className="typo-normal breadcrumbs__item breadcrumbs__current"
             >
-                Økonomisk sosialhjelp
-            </a> / {tittel}
-        </div>
-    )
+                {tittel}
+            </div>
+
+        </>
+    );
+
+    if (width && width < 576) {
+        crumbs = (
+            <>
+                <div key="chevron" aria-hidden={true}>
+                    <NavFrontendChevron type="venstre"/>
+                </div>
+                <p className="typo-normal breadcrumbs__item">
+                    <a
+                        href=".."
+                        title="Gå til forrige side"
+                        onClick={(event: any) => onClickLink(event, frontpageUrl)}
+                    >
+                        Tilbake
+                    </a>
+                </p>
+            </>
+        )
+    }
+
+    return (
+        <nav aria-label="Du er her" className="breadcrumbs">
+            {crumbs}
+        </nav>
+    );
 };
 
 export default Brodsmulesti;
