106635d3999dcebe41c01d375fa4e5cd481b5238
Thomas Flemming <thomas.flemming@gmail.com>
2019-12-10 16:40:25 +0100

    Ikke refreshe nettleser ved klikk på lenker under andre muligheter

--- src/komponenter/infopanel/LenkepanelLinje.tsx
+++ src/komponenter/infopanel/LenkepanelLinje.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
+import {onClickLink} from "../../utils/navigasjon";
 
 export const LenkeboksLinje = (props: {href: string; children: any}) => (
     <li className="lenkeboks-linje">
-        <a href={props.href}>{props.children}</a>
+        <a href={props.href} onClick={(event: any) => onClickLink(event, props.href)}>{props.children}</a>
     </li>
 );
