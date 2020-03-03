79f072d123965a455b3677321a8b08921cfb90e1
Halvor Bjørn <halvor.bjorn@nav.no>
2020-03-03 09:39:15 +0100

    Legger til engelske og nynorske tekster på forsiden

--- src/artikler/forside/Forside.tsx
+++ src/artikler/forside/Forside.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 import {detekterSprak, Sprak} from "../../utils/sprakUtils";
 import {Oversettelser} from "../../komponenter/oversettelser/Oversettelser";
 import {ForsideBokmal} from "./ForsideBokmal";
-//import {ForsideNynorsk} from "./ForsideNynorsk";
-//import {ForsideEnglish} from "./ForsideEnglish";
+import {ForsideNynorsk} from "./ForsideNynorsk";
+import {ForsideEnglish} from "./ForsideEnglish";
 
 const Forside = () => {
     const valgtSprak: string = detekterSprak();
@@ -13,8 +13,8 @@ const Forside = () => {
             sprak={[Sprak.NORSK_BOKMAL, Sprak.NYNORSK, Sprak.ENGELSK]}
         >
             {valgtSprak === Sprak.NORSK_BOKMAL && <ForsideBokmal />}
-            {valgtSprak === Sprak.NYNORSK && <ForsideBokmal />}
-            {valgtSprak === Sprak.ENGELSK && <ForsideBokmal />}
+            {valgtSprak === Sprak.NYNORSK && <ForsideNynorsk />}
+            {valgtSprak === Sprak.ENGELSK && <ForsideEnglish />}
         </Oversettelser>
     );
 };
