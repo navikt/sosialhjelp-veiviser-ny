cbfecd32c61862cb6c61a1720ac8d534b39626cc
thomasfl <thomas.flemming@gmail.com>
2020-03-25 13:00:36 +0100

    Setter KRISE=false inntil videre

--- src/artikler/sok-sosialhjelp/SokSosialhjelp.tsx
+++ src/artikler/sok-sosialhjelp/SokSosialhjelp.tsx
@@ -3,18 +3,43 @@ import {detekterSprak, Sprak} from "../../utils/sprakUtils";
 import {Oversettelser} from "../../komponenter/oversettelser/Oversettelser";
 import SokSosialhjelpBokmalKrise from "./SokSosialhjelpBokmalKrise";
 import SokSosialhjelpEngelskKrise from "./SokSosialhjelpEngelskKrise";
-import "./sokSosialhjelp.less";
 import SokSosialhjelpNynorskKrise from "./SokSosialhjelpNynorskKrise";
+import SokSosialhjelpNynorsk from "./SokSosialhjelpNynorsk";
+import SokSosialhjelpBokmal from "./SokSosialhjelpBokmal";
+import SokSosialhjelpEngelsk from "./SokSosialhjelpEngelsk";
+import "./sokSosialhjelp.less";
 
 const SokSosialhjelp: React.FC = () => {
     const valgtSprak: string = detekterSprak();
+
+    const KRISE = false;
+
     return (
         <Oversettelser
             sprak={[Sprak.NORSK_BOKMAL, Sprak.NYNORSK, Sprak.ENGELSK]}
         >
-            {valgtSprak === Sprak.NORSK_BOKMAL && <SokSosialhjelpBokmalKrise />}
-            {valgtSprak === Sprak.NYNORSK && <SokSosialhjelpNynorskKrise />}
-            {valgtSprak === Sprak.ENGELSK && <SokSosialhjelpEngelskKrise />}
+            {!KRISE && (
+                <>
+                    {valgtSprak === Sprak.NORSK_BOKMAL && (
+                        <SokSosialhjelpBokmal />
+                    )}
+                    {valgtSprak === Sprak.NYNORSK && <SokSosialhjelpNynorsk />}
+                    {valgtSprak === Sprak.ENGELSK && <SokSosialhjelpEngelsk />}
+                </>
+            )}
+            {KRISE && (
+                <>
+                    {valgtSprak === Sprak.NORSK_BOKMAL && (
+                        <SokSosialhjelpBokmalKrise />
+                    )}
+                    {valgtSprak === Sprak.NYNORSK && (
+                        <SokSosialhjelpNynorskKrise />
+                    )}
+                    {valgtSprak === Sprak.ENGELSK && (
+                        <SokSosialhjelpEngelskKrise />
+                    )}
+                </>
+            )}
         </Oversettelser>
     );
 };
