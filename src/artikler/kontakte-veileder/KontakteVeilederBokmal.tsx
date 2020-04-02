e89e67f52b6671e99e8e8e8244b9482f1d840012
thomasfl <thomas.flemming@gmail.com>
2020-04-02 10:48:42 +0200

    Ny tekst på kontakte veileder side

--- src/artikler/kontakte-veileder/KontakteVeilederBokmal.tsx
+++ src/artikler/kontakte-veileder/KontakteVeilederBokmal.tsx
@@ -11,8 +11,27 @@ const KontakteVeilederBokmal: React.FC = () => {
                 Hvordan kontakter du veilederen din?
             </Innholdstittel>
             <Normaltekst>
-                Hvis du trenger å snakke med noen om situasjonen din eller saken
-                din, kan du{" "}
+                Har du spørsmål i forbindelse med søknaden eller saken din om økonomisk sosialhjelp,
+                så kan du bruke{" "}
+                <Lenke href={"https://tjenester.nav.no/mininnboks/sporsmal/skriv/OKSOS"}>skriv til oss</Lenke>
+                {" "}
+                og velge området sosiale tjenester. Meldingen vil bli sendt til NAV-kontoret{" "}
+                der du er folkeregistrert. Hvis du oppholder deg i en annen kommune enn der du er{" "}
+                folkeregistrert, ber vi deg{" "}
+                <Lenke
+                    href={
+                        "https://www.nav.no/no/nav-og-samfunn/kontakt-nav/kontakt-nav-pa-telefon2"
+                    }
+                >
+                    kontakte oss på telefon
+                </Lenke>.
+            </Normaltekst>
+            <br/>
+            <Normaltekst>
+                Hvis du trenger å snakke med noen om situasjonen din eller saken din,  kan du melde behovet
+                for kontakt i{" "}
+                <Lenke href={"https://tjenester.nav.no/mininnboks/sporsmal/skriv/OKSOS"}>skriv til oss</Lenke>.
+                Da kontakter vi deg på telefon. Du kan også{" "}
                 <Lenke
                     href={
                         "https://www.nav.no/no/nav-og-samfunn/kontakt-nav/kontakt-nav-pa-telefon2"
@@ -23,8 +42,7 @@ const KontakteVeilederBokmal: React.FC = () => {
                 for å avtale et møte. Du kan lese mer om{" "}
                 <Lenke href="./slik-foregar-et-mote?lang=nb">
                     hvordan et møte foregår med oss
-                </Lenke>
-                .
+                </Lenke>.
             </Normaltekst>
         </Artikkel>
     );
