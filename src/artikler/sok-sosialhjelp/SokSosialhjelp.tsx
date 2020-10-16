6b6a7e5ab7aaee6ae8bcdf9847592b366a628906
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-10-16 11:37:24 +0200

    Fjerner SokSosialhjelpForskudd sidene da denne featuren aldri ble satt i prod i søknaden (#107)

--- src/artikler/sok-sosialhjelp/SokSosialhjelp.tsx
+++ src/artikler/sok-sosialhjelp/SokSosialhjelp.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import {detekterSprak, Sprak} from "../../utils/sprakUtils";
 import {Oversettelser} from "../../komponenter/oversettelser/Oversettelser";
-import SokSosialhjelpBokmalForskudd from "./SokSosialhjelpBokmalForskudd";
-import SokSosialhjelpEngelskForskudd from "./SokSosialhjelpEngelskForskudd";
-import SokSosialhjelpNynorskForskudd from "./SokSosialhjelpNynorskForskudd";
 import SokSosialhjelpNynorsk from "./SokSosialhjelpNynorsk";
 import SokSosialhjelpBokmal from "./SokSosialhjelpBokmal";
 import SokSosialhjelpEngelsk from "./SokSosialhjelpEngelsk";
@@ -14,34 +11,13 @@ export const ANTALL_KOMMUNER = 356;
 const SokSosialhjelp: React.FC = () => {
     const valgtSprak: string = detekterSprak();
 
-    const KORONAFORSKUDD = false;
-
     return (
         <Oversettelser
             sprak={[Sprak.NORSK_BOKMAL, Sprak.NYNORSK, Sprak.ENGELSK]}
         >
-            {!KORONAFORSKUDD && (
-                <>
-                    {valgtSprak === Sprak.NORSK_BOKMAL && (
-                        <SokSosialhjelpBokmal />
-                    )}
-                    {valgtSprak === Sprak.NYNORSK && <SokSosialhjelpNynorsk />}
-                    {valgtSprak === Sprak.ENGELSK && <SokSosialhjelpEngelsk />}
-                </>
-            )}
-            {KORONAFORSKUDD && (
-                <>
-                    {valgtSprak === Sprak.NORSK_BOKMAL && (
-                        <SokSosialhjelpBokmalForskudd />
-                    )}
-                    {valgtSprak === Sprak.NYNORSK && (
-                        <SokSosialhjelpNynorskForskudd />
-                    )}
-                    {valgtSprak === Sprak.ENGELSK && (
-                        <SokSosialhjelpEngelskForskudd />
-                    )}
-                </>
-            )}
+            {valgtSprak === Sprak.NORSK_BOKMAL && <SokSosialhjelpBokmal />}
+            {valgtSprak === Sprak.NYNORSK && <SokSosialhjelpNynorsk />}
+            {valgtSprak === Sprak.ENGELSK && <SokSosialhjelpEngelsk />}
         </Oversettelser>
     );
 };
