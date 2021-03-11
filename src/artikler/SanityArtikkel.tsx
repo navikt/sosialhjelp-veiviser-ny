f712575b30a477f937e9138e5879094c43937dad
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-03-11 13:55:24 +0100

    Catcher feil fra SanityClient og viser en enkel feilside dersom vi ikke klarer å hente data fra Sanity

--- src/artikler/SanityArtikkel.tsx
+++ src/artikler/SanityArtikkel.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as Sentry from "@sentry/browser";
 import {Innholdstittel} from "nav-frontend-typografi";
 import Artikkel from "./Artikkel";
 import {SanityBlockContent} from "../komponenter/SanityBlockContent";
@@ -10,13 +11,26 @@ import {Lastestriper} from "../komponenter/Lastestriper";
 
 const SanityArtikkel = (props: {slug: string; locale: "nb" | "nn" | "en"}) => {
     const [article, setArticle] = React.useState<SanityArticle>();
+    const [hasErros, setHasErrors] = React.useState(false);
     React.useEffect(() => {
-        fetchArticleWithSlugAndLocale(props.slug, props.locale).then(
-            (article) => {
+        fetchArticleWithSlugAndLocale(props.slug, props.locale)
+            .then((article) => {
                 setArticle(article);
-            }
-        );
+            })
+            .catch((e) => {
+                setHasErrors(true);
+                Sentry.captureException(e);
+            });
     }, [props.slug, props.locale, setArticle]);
+
+    if (hasErros) {
+        return (
+            <Artikkel tittel="Det har oppstått en feil">
+                <Innholdstittel>Det har oppstått en feil</Innholdstittel>
+                Du kan laste siden på nytt, eller prøve igjen senere.
+            </Artikkel>
+        );
+    }
 
     if (!article) {
         return (
