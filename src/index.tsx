22e7c9c684cbabdb141560a6377c7f2c0bc0471c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-05-19 13:21:56 +0200

    Rydder i utils

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,13 @@ import {render} from "react-dom";
 import * as Sentry from "@sentry/browser";
 import {v4 as uuid} from "uuid";
 
-import "./index.less";
 import {Router, Route, Switch, Redirect} from "react-router-dom";
-import {history} from "./utils/navigasjon";
 
-import {erProd, erQ} from "./utils/restUtils";
+import {erProd} from "./utils/restUtils";
 
 if (erProd()) {
     Sentry.init({
         dsn: "https://8c95ef33f4ec40abbd167d021f997637@sentry.gc.nav.no/52",
-    });
-} else if (erQ()) {
-    Sentry.init({
-        dsn: "https://92f71d949aac44bbbef3792447677d7c@sentry.gc.nav.no/16",
     });
 }
 Sentry.setUser({ip_address: "", id: uuid()});
