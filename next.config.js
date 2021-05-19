ebf9f9bf2a05453aa9d63aa29d3863a2d1ef1418
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-05-19 10:08:48 +0200

    Legger til redirect til familie

--- next.config.js
+++ next.config.js
@@ -16,6 +16,11 @@ const withTranspileModules = require("next-transpile-modules")(
 const redirects = [
     {source: "/artikkel/124876", destination: "/gi-beskjed", permanent: true},
     {source: "/artikkel/124875", destination: "/klage", permanent: true},
+    {
+        source: "/hvis-du-er-enslig-forsorger",
+        destination: "https://www.nav.no/familie/alene-med-barn",
+        permanent: true,
+    },
 ];
 
 module.exports = withTranspileModules(
