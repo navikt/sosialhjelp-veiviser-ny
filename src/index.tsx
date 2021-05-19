fd74aef4ae5ac5e23a188620c7504c38994832c2
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-05-19 13:04:16 +0200

    Fjerner resterende artikler

--- src/index.tsx
+++ src/index.tsx
@@ -6,25 +6,11 @@ import {render} from "react-dom";
 import * as Sentry from "@sentry/browser";
 import {v4 as uuid} from "uuid";
 
-import DetteBorDuVite from "./artikler/dette-bor-du-vite/DetteBorDuVite";
 import "./index.less";
 import {Router, Route, Switch, Redirect} from "react-router-dom";
-import DetteKanDuSokeOm from "./artikler/dette-kan-du-soke-om/DetteKanDuSokeOm";
 import {history} from "./utils/navigasjon";
-import Forside from "./artikler/forside/Forside";
-import SokSosialhjelp from "./artikler/sok-sosialhjelp/SokSosialhjelp";
-import Nodsituasjon from "./artikler/nodsituasjon/Nodsituasjon";
-import GiBeskjed from "./artikler/gi-beskjed/GiBeskjed";
-import Klage from "./artikler/klage/Klage";
-import KravTilDeg from "./artikler/krav-til-deg/KravTilDeg";
-import SoknadPaPapir from "./artikler/soknad-pa-papir/SoknadPaPapir";
-import Ettersende from "./artikler/ettersende/Ettersende";
-import Behandlingstid from "./artikler/behandlingstid/Behandlingstid";
-import KontakteVeileder from "./artikler/kontakte-veileder/KontakteVeileder";
-import Korona from "./artikler/korona/Korona";
-import StatusSoknad from "./artikler/status-soknad/StatusSoknad";
+
 import {erProd, erQ} from "./utils/restUtils";
-import SanityArtikkel from "./artikler/SanityArtikkel";
 
 if (erProd()) {
     Sentry.init({
@@ -38,100 +24,7 @@ if (erProd()) {
 Sentry.setUser({ip_address: "", id: uuid()});
 
 function App() {
-    return (
-        <div id="app" className="app">
-            <Router history={history}>
-                <Switch>
-                    <Route exact path="/" component={Forside} />
-                    <Route
-                        exact
-                        path="/dette-bor-du-vite"
-                        component={DetteBorDuVite}
-                    />
-                    <Route
-                        exact
-                        path="/dette-kan-du-soke-om"
-                        component={DetteKanDuSokeOm}
-                    />
-                    <Route exact path="/gi-beskjed" component={GiBeskjed} />
-                    <Route exact path="/klage" component={Klage} />
-                    <Route exact path="/krav-til-deg" component={KravTilDeg} />
-                    <Route
-                        exact
-                        path="/sok-sosialhjelp"
-                        component={SokSosialhjelp}
-                    />
-                    <Route
-                        exact
-                        path="/slik-soker-du"
-                        component={SokSosialhjelp}
-                    />
-                    {/* Et alias */}
-                    <Route
-                        exact
-                        path="/nodsituasjon"
-                        component={Nodsituasjon}
-                    />
-
-                    <Route exact path="/sok-papir" component={SoknadPaPapir} />
-
-                    <Route
-                        exact
-                        path="/behandlingstid"
-                        component={Behandlingstid}
-                    />
-
-                    <Route
-                        exact
-                        path="/status-soknad"
-                        component={StatusSoknad}
-                    />
-
-                    <Route exact path="/ettersende" component={Ettersende} />
-
-                    <Route
-                        exact
-                        path="/kontakte-veileder"
-                        component={KontakteVeileder}
-                    />
-
-                    <Route exact path="/korona" component={Korona} />
-
-                    <Route
-                        exact
-                        path="/sanity-test"
-                        component={SanityArtikkel}
-                    />
-
-                    {/* Redirects */}
-                    <Route exact path="/artikkel/514891">
-                        <Redirect to="/nodsituasjon" />
-                    </Route>
-                    <Route exact path="/artikkel/514874">
-                        <Redirect to="/hvis-du-har-barn" />
-                    </Route>
-                    <Route exact path="/artikkel/514879">
-                        <Redirect to="/hvis-du-har-samboer" />
-                    </Route>
-                    <Route exact path="/artikkel/514878">
-                        <Redirect to="/hvis-du-er-gift" />
-                    </Route>
-                    <Route exact path="/artikkel/514876">
-                        <Redirect to="/slik-foregar-et-mote" />
-                    </Route>
-                    <Route exact path="/artikkel/124876">
-                        <Redirect to="/gi-beskjed" />
-                    </Route>
-                    <Route exact path="/artikkel/514875">
-                        <Redirect to="/krav-til-deg" />
-                    </Route>
-                    <Route exact path="/artikkel/124875">
-                        <Redirect to="/klage" />
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+    return <div id="app" className="app"></div>;
 }
 
 const rootElement = document.getElementById("root");
