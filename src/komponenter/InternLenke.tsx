81d5e5255dc1685245f9a46a2613e9f5aa4d133a
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-05-19 13:07:43 +0200

    Fjerner SokDigitalt komponent som var skrevet for CRA

--- src/komponenter/InternLenke.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import {Link} from "react-router-dom";
-
-export const InternLenke = (props: {href: string; children: any}) => (
-    <Link to={props.href} className="lenke">
-        {props.children}
-    </Link>
-);
