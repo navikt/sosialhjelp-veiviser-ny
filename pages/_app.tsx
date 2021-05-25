21d31fe941318d48088d32504f2d63ab2e04bcf2
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-05-25 15:18:56 +0200

    Legger til støtte for sentry i nextjs versjon av appen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,23 @@ import "nav-frontend-lenker-style";
 import "nav-frontend-knapper-style";
 
 import {useEffect} from "react";
+import * as Sentry from "@sentry/browser";
+import {v4 as uuid} from "uuid";
 
 import {initAmplitude} from "../src/utils/amplitude";
 
 function App({Component, pageProps}) {
     useEffect(() => {
         initAmplitude();
+        if (
+            window?.location?.href.startsWith("https://www.nav.no/sosialhjelp")
+        ) {
+            Sentry.init({
+                dsn:
+                    "https://8c95ef33f4ec40abbd167d021f997637@sentry.gc.nav.no/52",
+            });
+            Sentry.setUser({ip_address: "", id: uuid()});
+        }
     }, []);
 
     return <Component {...pageProps} />;
