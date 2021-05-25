879c118c11bd407b418edddea56e963488d90874
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-05-25 15:15:20 +0200

    Legger til Amplitude og teller bruk av kommunesøket (#245)
    Legger til Amplitude og teller bruk av kommunesøket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "nav-frontend-lenker-style";
 import "nav-frontend-knapper-style";
 
+import {useEffect} from "react";
+
+import {initAmplitude} from "../src/utils/amplitude";
+
 function App({Component, pageProps}) {
+    useEffect(() => {
+        initAmplitude();
+    }, []);
+
     return <Component {...pageProps} />;
 }
 
