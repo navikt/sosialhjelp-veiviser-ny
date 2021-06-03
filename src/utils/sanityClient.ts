4123a60458892b6ee3fb3f81c3aac3630d2137ad
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-06-03 10:38:23 +0200

    Bruker dataset fra env-variabel i stedet for å sjekke på om appen kjører i prod (#256)

--- src/utils/sanityClient.ts
+++ src/utils/sanityClient.ts
@@ -1,11 +1,10 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 import {SanityImageSource} from "@sanity/image-url/lib/types/types";
-import {erProd} from "./restUtils";
 
 const client = sanityClient({
     projectId: "hvfvg2j3",
-    dataset: erProd() ? "production" : "test",
+    dataset: process.env.SANITY_DATASET ?? "production",
     useCdn: true,
     apiVersion: "2021-03-26",
 });
