93aa63b53683c1ef7cb5842e8cd6977ba3bde89b
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-07-01 11:56:46 +0200

    Justerer antall kolonner i grid

--- components/Content.tsx
+++ components/Content.tsx
@@ -5,11 +5,11 @@ export const Content = (props: {children}) => {
     return (
         <ContentContainer>
             <Grid>
-                <Cell xs={12} md={1} lg={2}></Cell>
-                <Cell xs={12} md={10} lg={8}>
+                <Cell xs={12} lg={2}></Cell>
+                <Cell xs={12} lg={8}>
                     {props.children}
                 </Cell>
-                <Cell xs={12} md={1} lg={2}></Cell>
+                <Cell xs={12} lg={2}></Cell>
             </Grid>
         </ContentContainer>
     );
