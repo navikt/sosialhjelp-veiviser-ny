b3cc83acf84cd6b9fadc94b3e053579fe19c5e9c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-09-24 13:31:21 +0200

    Trekker ut datasett og projectId til env-variabler (#295)

--- src/utils/sanityClient.ts
+++ src/utils/sanityClient.ts
@@ -1,18 +1,13 @@
 import sanityClient from "@sanity/client";
-import imageUrlBuilder from "@sanity/image-url";
-import {SanityImageSource} from "@sanity/image-url/lib/types/types";
+import {createPreviewSubscriptionHook} from "next-sanity";
 
-const client = sanityClient({
-    projectId: "hvfvg2j3",
-    dataset: process.env.SANITY_DATASET ?? "production",
+const config = {
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     useCdn: true,
     apiVersion: "2021-08-30",
-});
-
-const builder = imageUrlBuilder(client);
-
-export const urlFor = (source: SanityImageSource) => {
-    return builder.image(source);
 };
 
-export default client;
+export default sanityClient(config);
+
+export const usePreviewSubscription = createPreviewSubscriptionHook(config);
