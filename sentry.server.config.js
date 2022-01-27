f5e2cbadcdc8f288ab0ce373bac3fa519aa1ac4c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-01-27 13:07:29 +0100

    Fjerner prod-sbs

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -12,7 +12,7 @@ Sentry.init({
         "https://b4c26d0d3152462888c2af9c8a43be15@sentry.gc.nav.no/98",
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
-    environment: process.env.NEXT_PUBLIC_ENVIRONMENT ?? "prod-sbs",
+    environment: process.env.NEXT_PUBLIC_ENVIRONMENT ?? "prod-gcp",
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
