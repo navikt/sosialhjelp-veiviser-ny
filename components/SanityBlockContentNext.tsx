8be1aad9e340524af55b4384dae105f8855b6e4e
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-02-17 12:50:12 +0100

    Fjerner ubrukt import

--- components/SanityBlockContentNext.tsx
+++ components/SanityBlockContentNext.tsx
@@ -6,7 +6,6 @@ import {
 import React, {useContext} from "react";
 import Link from "next/link";
 import Vimeo from "@u-wave/react-vimeo";
-import client from "../src/utils/sanityClient";
 import styled from "styled-components";
 import {EmbeddedVideo} from "./article/EmbeddedVideo";
 import {
